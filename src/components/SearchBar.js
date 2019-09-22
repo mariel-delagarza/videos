ff41602ace6144cee9182af7f69098332f63f39d
mariel-delagarza <mariel.code@gmail.com>
2019-09-21 22:41:41 -0400

    Create onFormSubmit callback function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,13 +9,19 @@ class SearchBar extends React.Component {
   onInputChange = (event) => {
     this.setState({term: event.target.value});
   };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+
+    //TODO: Make sure we call callback from parent component
+  };
   /*onChange is a special name for the attribute below and gets called 
    any time input gets changed; if we do not use it for the callback 
    function we will not be able to change the value of our state*/
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input 
