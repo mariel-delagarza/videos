5b271c992b4ba131bc002f908f9621b6b3f35fc3
mariel-delagarza <mariel.code@gmail.com>
2019-09-22 17:55:01 -0400

    Resolved error from axios requiring a 'part' parameter

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 const KEY = 'AIzaSyBf3I2PZslF_TNPzOrOVOvqfrkGVPqUowI'
 
+export const baseParams = { 
+  part: "snippet",
+  maxResults: 5,
+  key: KEY 
+};
+
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
-  params: {
-    part: 'snippet',
-    maxResults: 5,
-    key: KEY 
-  }
+  params: baseParams
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
+import youtube, { baseParams } from '../apis/youtube';
 
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get('/search', {
+  state = { videos:  []};
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
       params: {
+        ...baseParams,
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />  
+        I have {this.state.videos.length} videos.
       </div>
     );
   }
