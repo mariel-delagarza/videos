191405cb538ac01bf0ac5996f805721813bd7de8
mariel-delagarza <mariel.code@gmail.com>
2019-09-21 22:39:44 -0400

    Added onChange to the form input tag and created callback function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,13 +6,22 @@ class SearchBar extends React.Component {
   we are saving our data inside the component and not inside the DOM*/
   state = { term: ''};
 
+  onInputChange = () => {
+    
+  }
+  /*onChange is a special name for the attribute below and gets called 
+   any time input gets changed; if we do not use it for the callback 
+   function we will not be able to change the value of our state*/
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" value={this.state.term} />
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={this.onInputChange}/>
           </div>
         </form>
       </div>
