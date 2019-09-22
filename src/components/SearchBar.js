bbc0a11c2cd5d0f0c3dc4eb9a8fd2c23ca19b522
mariel-delagarza <mariel.code@gmail.com>
2019-09-21 22:19:47 -0400

    Initialize state and add it as a value to the input field

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+  /*Utilizing state turns this into a controlled component and ensures
+  we are saving our data inside the component and not inside the DOM*/
+  state = { term: ''};
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" />
+            <input type="text" value={this.state.term} />
           </div>
         </form>
       </div>
