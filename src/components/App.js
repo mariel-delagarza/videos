045735a1c3784dc37a52de30c802005dc18f7a34
mariel-delagarza <mariel.code@gmail.com>
2019-09-22 21:24:27 -0400

    Add VideoDetail component to the render method

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube, { baseParams } from '../apis/youtube';
 import VideoList from './VideoList'
-
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = { videos:  [], selectedVideo: null};
@@ -26,6 +26,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />  
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     );
