228a274801e822e6e0dfdebec37c6dd00e91ca8e
mariel-delagarza <mariel.code@gmail.com>
2019-09-22 20:14:18 -0400

    Additional styling using semantic-ui

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,8 +3,11 @@ import React from 'react';
 const VideoItem = ({ video }) => {
   return (
   <div className="item">
-    <img src={video.snippet.thumbnails.medium.url} />
-    {video.snippet.title}
+    <img src={video.snippet.thumbnails.medium.url} className="ui image" />
+    <div className="content">
+      <div className="header">{video.snippet.title}</div>
+    </div>
+    
   </div>
   );
 };
