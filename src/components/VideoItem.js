af751dea4665416e621689694a2d73e2a50c805a
mariel-delagarza <mariel.code@gmail.com>
2019-09-22 20:16:46 -0400

    Create css file and import into VideoItem to apply custom CSS styling changing the size of the thumbnail and center the titles

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './VideoItem.css';
 
 const VideoItem = ({ video }) => {
   return (
-  <div className="item">
+  <div className="video-item item">
     <img src={video.snippet.thumbnails.medium.url} className="ui image" />
     <div className="content">
       <div className="header">{video.snippet.title}</div>
