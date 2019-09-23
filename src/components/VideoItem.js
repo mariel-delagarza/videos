c2c3384cf3bd46c1324b5fffcd115bb6c6b69793
mariel-delagarza <mariel.code@gmail.com>
2019-09-22 21:10:03 -0400

    Add in onVideoSelect as an arrow function to make sure that it is receiving the correct video

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-  <div className="video-item item">
+  <div onClick={() => onVideoSelect(video)} className="video-item item">
     <img src={video.snippet.thumbnails.medium.url} className="ui image" />
     <div className="content">
       <div className="header">{video.snippet.title}</div>
